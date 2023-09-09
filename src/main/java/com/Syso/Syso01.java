44a39bdd9881bd426044ce21df25cdb33a090af8
ygdalparslan <ygdalparslan@gmail.com>
2023-09-09 23:45:28 +0300

    syso01

---
+++ src/main/java/com/Syso/Syso01.java
@@ -0,0 +1,22 @@
+package com.Syso;
+
+public class Syso01 {
+            /*
+            Ilk satira Hello
+            ikinci satira World!
+            yazdiran praogramin java kodunu yaziniz.
+
+            Ornek Cikti:
+            Hello
+            World!
+             */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nWorld");
+
+
+    }
+
+
+}
