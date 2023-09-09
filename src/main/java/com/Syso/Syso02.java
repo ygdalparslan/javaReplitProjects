9774c847465125a328f8bdbd5ad00439bb923647
ygdalparslan <ygdalparslan@gmail.com>
2023-09-10 00:07:17 +0300

    syso02

---
+++ src/main/java/com/Syso/Syso02.java
@@ -0,0 +1,21 @@
+package com.Syso;
+
+public class Syso02 {
+    /*
+
+         Iki integer'i toplayan java programini yaziniz.
+
+        int a=10
+        int b=14
+        Ornek Cikti:
+        Sayilarin Toplami : 24
+
+     */
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 14;
+        System.out.println("Sayıların Toplamı : "+(a+b));
+
+    }
+}
