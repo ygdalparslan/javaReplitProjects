dc8b6eabe192a1cc8bac728c24b5e8739194fbcc
ygdalparslan <ygdalparslan@gmail.com>
2023-09-10 10:43:13 +0300

    syso04

---
+++ src/main/java/com/Syso/Syso04.java
@@ -0,0 +1,31 @@
+package com.Syso;
+
+    /*
+    Dairenin alanini ve cevresini hesaplayan java kodunu yaziniz.
+
+        r=7;
+        Pi=3.14
+        Ipucu:
+        Dairenin Cevresi : 2*Pi*r
+        Dairenin Alani : Pi*r*r
+        Ornek Cikti:
+        43.96
+        153.86
+     */
+
+public class Syso04 {
+
+    public static void main(String[] args) {
+
+        int r = 7;
+        double pi = 3.14;
+
+        System.out.println("Dairenin Cevresi : "+2*pi*r);
+        System.out.println("Dairenin Alani : "+pi*r*r);
+
+
+
+
+
+    }
+}
