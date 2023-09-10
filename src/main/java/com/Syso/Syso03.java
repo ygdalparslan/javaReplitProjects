7ee67deceb65407b6e8d10df6fcd77fd978f0454
ygdalparslan <ygdalparslan@gmail.com>
2023-09-10 10:38:44 +0300

    syso03

---
+++ src/main/java/com/Syso/Syso03.java
@@ -0,0 +1,32 @@
+package com.Syso;
+
+    /*
+        Verilen integer sayilarin
+        -Toplamini,
+        -carpimini,
+        -cikarma islemini ve
+        -bolme islemini yapan java kodunu yaziniz.
+
+        num1=100;
+        num2=25;
+        Ornek Cikti:
+        125
+        2500
+        75
+        4
+     */
+public class Syso03 {
+
+        public static void main(String[] args) {
+
+            int num1 = 100;
+            int num2 = 25;
+
+            System.out.println(num1+num2);
+            System.out.println(num1*num2);
+            System.out.println(num1-num2);
+            System.out.println(num1/num2);
+        }
+
+
+}
