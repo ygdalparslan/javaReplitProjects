8e0bb30fd278b7bcba205b6344642def6b194667
ygdalparslan <ygdalparslan@gmail.com>
2023-09-10 10:56:22 +0300

    syso06

---
+++ src/main/java/com/Syso/Syso06.java
@@ -0,0 +1,23 @@
+package com.Syso;
+
+/*
+  Verilen 5 sayinin ortalamasiniz hesaplayan java kodunu yaziniz.
+      Ortalamasi alinacak Sayilar : 22, 20, 30, 28, 50
+      Ornek Cikti:
+      Sayilarin Ortalamasi : 30
+
+  */
+public class Syso06 {
+
+    public static void main(String[] args) {
+
+        int num1 = 22;
+        int num2 = 20;
+        int num3 = 30;
+        int num4 = 28;
+        int num5 = 50;
+
+        System.out.println("Sayilarin Ortalamasi : "+(num1+num2+num3+num4+num5)/5);
+
+    }
+}
