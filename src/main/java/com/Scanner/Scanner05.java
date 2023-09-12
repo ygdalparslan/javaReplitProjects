dd43a1d23a4c465fa86a5679750b2b5572fa30dc
ygdalparslan <ygdalparslan@gmail.com>
2023-09-12 14:13:59 +0300

    scanner05

---
+++ src/main/java/com/Scanner/Scanner05.java
@@ -0,0 +1,28 @@
+package com.Scanner;
+
+import java.util.Scanner;
+
+public class Scanner05 {
+
+    /*
+      Dikdortgenin alanini ve cevresini hesaplayan java kodunu yaziniz.
+        A=5
+        B=3
+        Ipucu:
+        Dikdortgenin Cevresi : 2*(A+B);
+        Dikdortgenin Alani : A * B ;
+        Ornek Cikti:
+        Dikdortgenin Cevresi : 16
+        Dikdortgenin Alani : 15
+    */
+
+    public static void main(String[] args) {
+
+        int a =5;
+        int b =3;
+
+        System.out.println("Dikdortgenin Cevresi : "+2*(a+b));
+        System.out.println("Dikdortgenin Alani : "+a*b);
+
+    }
+}
