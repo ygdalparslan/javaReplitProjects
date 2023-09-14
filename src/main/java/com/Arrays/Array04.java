bc5f2323f8eafb8bd58e4503079ad4304a5f7ce2
ygdalparslan <ygdalparslan@gmail.com>
2023-09-14 23:28:57 +0300

    Array04

---
+++ src/main/java/com/Arrays/Array04.java
@@ -0,0 +1,35 @@
+package com.Arrays;
+/*
+Aşağıdaki grid şeklini yazan Java Kodunu yazınız.
+
+```
+Beklenen Çıktı:
+```
+
+```
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0
+```
+ */
+
+public class Array04 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <10 ; i++) {
+            for (int j = 0; j <10; j++) {
+                System.out.print("0  ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
