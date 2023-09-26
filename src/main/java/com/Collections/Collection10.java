7e95919390645b0b633025991b607776532f0f3d
ygdalparslan <ygdalparslan@gmail.com>
2023-09-26 11:54:46 +0300

    collection10

---
+++ src/main/java/com/Collections/Collection10.java
@@ -0,0 +1,35 @@
+package com.Collections;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/*
+Bir LinkedList oluşturalım ve ilk eleman ile 4. elemanı yerdeğiştiren java kodunu yazınız.
+LinkedList: mavi,kirmizi,beyaz,siyah,yesil,turuncu
+**hint:  Collections.swap();**
+```
+Beklene Çıktı:
+``````
+Orjinal LinkedList: [mavi,kirmizi,beyaz,siyah,yesil,turuncu]
+``````
+Swap sonrası durum: [**siyah**,kirmizi,beyaz,**mavi**,yesil,turuncu]
+```
+ */
+public class Collection10 {
+    public static void main(String[] args) {
+
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("mavi");
+        colors.add("kirmizi");
+        colors.add("beyaz");
+        colors.add("siyah");
+        colors.add("yesil");
+        colors.add("turuncu");
+
+        System.out.println("\nOrjinal LinkedList: "+colors);
+
+        Collections.swap(colors,0,3);
+        System.out.println("\nSwap sonrası durum: "+colors);
+    }
+}
