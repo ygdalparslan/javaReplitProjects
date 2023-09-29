20b298c67cfd63d53d19ade2535c1a8aa411e324
ygdalparslan <ygdalparslan@gmail.com>
2023-09-29 22:45:47 +0300

    ConditionalStatement06

---
+++ src/main/java/com/ConditionalStatements/ConditionalStatement06.java
@@ -0,0 +1,31 @@
+package com.ConditionalStatements;
+
+/*
+1 den 20 ye kadar tam sayıları alt alta yazan Java kodu yazınız.
+
+Hint: For döngüsü kullanınız.
+```
+Beklenen Çıktı:
+``````
+1
+2
+3
+4
+
+.
+``````
+19
+``````
+20
+```
+ */
+public class ConditionalStatement06 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=20 ; i++) {
+            System.out.println(i+"\n``````");
+        }
+
+
+    }
+}
