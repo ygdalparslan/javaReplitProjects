e065367497edc51fe66ab53625a6ad684377ee09
ygdalparslan <ygdalparslan@gmail.com>
2023-09-29 22:53:07 +0300

    ConditionalStatement07

---
+++ src/main/java/com/ConditionalStatements/ConditionalStatement07.java
@@ -0,0 +1,36 @@
+package com.ConditionalStatements;
+/*
+Belirlenen sayının 1 den 10 a kadar çarpım sonuçlarını yapan Java programını yazınız.
+```
+Örnek Sonuç:
+```
+```
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+```
+ */
+
+import java.util.Scanner;
+
+public class ConditionalStatement07 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        for (int i = 1 ; i <=10 ; i++) {
+
+            System.out.println(num +" x "+i+" = "+num*i);
+
+        }
+
+
+    }
+}
